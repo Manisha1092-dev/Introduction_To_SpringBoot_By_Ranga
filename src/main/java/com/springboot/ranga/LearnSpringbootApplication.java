fae71d70b336286405371e1c15ab477dade456cf
Manisha Dhyani <dhyanimanishals95@gmail.com>
2025-07-22 12:50:15 +0530

    basic structure for SB project

---
+++ src/main/java/com/springboot/ranga/LearnSpringbootApplication.java
@@ -0,0 +1,11 @@
+package com.springboot.ranga;
+
+
+import org.springframework.boot.SpringApplication;
+
+public class LearnSpringbootApplication {
+    public static void main(String[] args) {
+
+        SpringApplication.run(LearnSpringbootApplication.class,args);
+    }
+}
